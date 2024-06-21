96a2ecd65a95b82897826cd0ca5482e8731be2b7
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2024-06-21 13:36:22 +0530

    Delete client directory

--- client/functions/index.jsx
+++
@@ -1,9 +0,0 @@
-import defaultAvatarUrl from "../public/connxprofile.png"; // Import the default avatar image directly
-
-export const imageSource = (user) => {
-  if (user && user.image && user.image.url) {
-    return user.image.url;
-  } else {
-    return defaultAvatarUrl; // Use the imported default avatar image
-  }
-};
